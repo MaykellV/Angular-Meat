e7a5c847609bc02e4f319e66ff8b43591dd7abb6
Vinicius Costa <vinicius.costa@opah.com.br>
2018-02-08 18:28:42 -0200

    Tela de Finalização da compra validada

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, ContentChild } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'mt-input-container',
   templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
 
   @Input() label: string
   @Input() errorMessage: string
@@ -24,5 +24,13 @@ export class InputComponent implements OnInit {
      if(this.input === undefined) {
        throw new Error('Esse componente precisa ser usado com uma diretiva ngModel');
      }
+  }
+
+  hasSucess(): boolean {
+    return this.input.valid && (this.input.dirty || this.input.touched)
+  }
+
+  hasError(): boolean {
+    return this.input.invalid && (this.input.dirty || this.input.touched)
   }
 }
