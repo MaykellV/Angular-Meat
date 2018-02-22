50ebb524988f0e875b4a841ceaacd3988ce588ad
Vinicius Costa <vinicius.costa@opah.com.br>
2018-02-22 15:48:49 -0300

    adicionando order component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { InputComponent } from 'app/shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+
 
 
 
@@ -43,8 +45,8 @@ import { OrderService } from './order/order.service';
     OrderComponent,
     InputComponent,
     RadioComponent,
-    OrderItemsComponent
-        
+    OrderItemsComponent,
+    DeliveryCostsComponent
   ],
   imports: [
     BrowserModule,
