080ef34623b1c1b1e53b03ec849140d164e54968
Vinicius Costa <vinicius.costa@opah.com.br>
2018-02-26 15:59:18 -0300

    componente de avaliação da compra implementado

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mt-rating',
   templateUrl: './rating.component.html'
 })
 export class RatingComponent implements OnInit {
+
+  @Output() rated = new EventEmitter<number>()
 
   rates: number[] = [1,2,3,4,5];
  
@@ -19,7 +21,22 @@ export class RatingComponent implements OnInit {
 
   setRate(r :number){
     this.rate = r;
-    console.log(r)
+    this.previusRate = undefined
+    this.rated.emit(this.rate)
+  }
+
+  setTemporaryRate(r: number) {
+    if(this.previusRate === undefined) {
+      this.previusRate = this.rate
+    }
+    this.rate = r
+  }
+
+  clearTemporaryRate() {
+    if(this.previusRate !== undefined) {
+      this.rate = this.previusRate
+      this.previusRate = undefined
+    }
   }
 
 }
